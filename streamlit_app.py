ad1f7ad4bcce56f16a6d329734f01fe6affc7fc6
stoyatwork <sophiastahltoyotawork@gmail.com>
2024-08-29 10:13:58 +0200

    Update streamlit_app.py
    Moved fruityvice request into for fruit chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     # st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -42,6 +44,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
